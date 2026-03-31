d2fd36fe3697330d45dca3f6fee9864356e092d3
Brando Elí Carrillo Pérez <77902818+becarrillo@users.noreply.github.com>
2026-03-31 14:55:24 -0500

    Remove NoSpaces validation decorator into register.request.dto.ts
    Removed NoSpaces custom validation decorator from register.request.dto.ts.

--- src/auth/dto/register.request.dto.ts
+++ src/auth/dto/register.request.dto.ts
@@ -26,21 +26,3 @@ export class RegisterRequestDto {
   })
   password: string;
 }
-
-import { registerDecorator, ValidationOptions } from 'class-validator';
-
-export function NoSpaces(validationOptions?: ValidationOptions) {
-  return function (object: object, propertyName: string) {
-    registerDecorator({
-      name: 'noSpaces',
-      target: object.constructor,
-      propertyName,
-      options: validationOptions,
-      validator: {
-        validate(value: unknown) {
-          return typeof value === 'string' && !/\s/g.test(value);
-        },
-      },
-    });
-  };
-}
