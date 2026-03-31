2f19da67839f396b99723b764e3f6b46b1f299b7
Brando Elí Carrillo Pérez <77902818+becarrillo@users.noreply.github.com>
2026-03-31 15:03:53 -0500

    Change RegisterDto to RegisterRequestDto dependency in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@
 
 import { Body, Controller, Post, BadRequestException } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { RegisterDto } from './dto/register.dto';
+import { RegisterRequestDto } from './dto/register.request.dto';
 import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
@@ -10,7 +10,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() dto: RegisterDto) {
+  async register(@Body() dto: RegisterRequestDto) {
     try {
       const result = await this.authService.register(dto);
       return {
