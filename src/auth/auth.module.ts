79e3802726065ab695211ff3c276f7882639c755
Azfe <alexzapata1984@gmail.com>
2026-05-30 03:13:52 +0200

    feat: (#92) add PasswordHashService to encapsulate Argon2

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,13 +5,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { AuthRepository } from './repositories/auth.repository';
 import { SupabaseAuthGuard } from './guards/supabase-auth.guard';
+import { PasswordHashService } from './password-hash.service';
 import { UsersModule } from '../modules/users/users.module';
 import { ObservabilityModule } from '../observability/observability.module';
 
 @Module({
   imports: [UsersModule, ObservabilityModule],
   controllers: [AuthController],
-  providers: [AuthService, AuthRepository, SupabaseAuthGuard],
-  exports: [SupabaseAuthGuard],
+  providers: [
+    AuthService,
+    AuthRepository,
+    SupabaseAuthGuard,
+    PasswordHashService,
+  ],
+  exports: [SupabaseAuthGuard, PasswordHashService],
 })
 export class AuthModule {}
